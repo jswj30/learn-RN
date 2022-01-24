0dfd238c02f34696da7bbd23e4150224d09fd04f
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-24 15:58:31 +0900

    3.1

--- 03/TodoApp/App.js
+++ 03/TodoApp/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import DateHead from './components/DateHead';
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import DateHead from './components/DateHead';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 const App = () => {
   const today = new Date();
   
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
-        <DateHead date={today} />
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <KeyboardAvoidingView
+          // behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          behavior={Platform.select({ios: 'padding', android: undefined})}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   )
 }
 
 const styles = StyleSheet.create({
-
-})
+  block: {
+    flex: 1, 
+    backgroundColor: 'white', 
+  }, 
+  avoid: {
+    flex: 1, 
+  }, 
+});
 
 export default App;
