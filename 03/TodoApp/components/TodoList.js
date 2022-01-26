c4d66f0778792f523e8de83cd49e06ef7abc7eff
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-26 17:16:35 +0900

    03

--- 03/TodoApp/components/TodoList.js
+++ 03/TodoApp/components/TodoList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
 
 const TodoList = ({todos}) => {
   return (
     <FlatList 
+    ItemSeparatorComponent={() => <View style={styles.separator} />}
       style={styles.list}
       data={todos}
       renderItem={({item}) => (
-        <View>
-          <Text>{item.text}</Text>
-        </View>
+        <TodoItem id={item.id} text={item.text} done={item.done} />
       )}
       keyExtractor={item => item.id.toString()}
     />
@@ -19,7 +19,11 @@ const TodoList = ({todos}) => {
 const styles = StyleSheet.create({
   list: {
     flex: 1, 
-  }
+  }, 
+  separator: {
+    backgroundColor: '#e0e0e0', 
+    height: 1, 
+  }, 
 })
 
 export default TodoList;
