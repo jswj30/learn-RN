1d71f5b351b4b5b1e8cec831a13fe2f00e086b34
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-27 19:12:01 +0900

    screen

---
+++ 05/LearnReactNavigation/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Button title="Detail 열기" />
+    </View>
+  )
+}
