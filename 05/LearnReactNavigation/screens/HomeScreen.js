bc351a9338d7eebf7dbd8595dae517d275b5dc17
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-28 11:31:53 +0900

    edit

--- 05/LearnReactNavigation/screens/HomeScreen.js
+++ 05/LearnReactNavigation/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Button} from 'react-native';
 
-function HomeScreen({navigation}) {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Button title="Detail 열기" />
     </View>
   )
 }
+
+export default HomeScreen;
