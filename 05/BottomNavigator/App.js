c99976b236214fa096ce19c5c792ceb4fc57aea9
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-31 20:37:28 +0900

    1

---
+++ 05/BottomNavigator/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return <Text>Home</Text>
+}
+
+function SearchScreen() {
+  return <Text>Search</Text>
+}
+
+function NotificationScreen() {
+  return <Text>Notification</Text>
+}
+
+function MessageScreen() {
+  return <Text>Message</Text>
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        initialRouteName='Home'
+        tabBarOptions={{
+          activeTintColor: '#fb8c00', 
+          showLabel: false, 
+        }}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            title: '홈', 
+            tabBarIcon: ({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            ), 
+          }}  
+        />
+        <Tab.Screen 
+          name="Search" 
+          component={SearchScreen} 
+          options={{
+            title: '검색', 
+            tabBarIcon: ({color, size}) => (
+              <Icon name="search" color={color} size={size} />
+            ), 
+          }}
+        />
+        <Tab.Screen 
+          name="Notification"
+          component={NotificationScreen} 
+          options={{
+            title: '알림', 
+            tabBarIcon: ({color, size}) => (
+              <Icon name="notifications" color={color} size={size} />
+            ), 
+          }}
+        />
+        <Tab.Screen 
+          name="Message" 
+          component={MessageScreen} 
+          options={{
+            title: '메시지', 
+            tabBarIcon: ({color, size}) => (
+              <Icon name="message" color={color} size={size} />
+            ), 
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ 05/BottomNavigator/screens/MainScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createBottomTabNavigator();
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>1234</Text>
+    </View>
+  );
+};
+
+export default MainScreen;
