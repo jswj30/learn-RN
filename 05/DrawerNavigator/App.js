d9b737e8a11ae5b838bf63c4dc31bac82b709748
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2022-01-31 19:51:33 +0900

    Drawer Navigator

--- 05/DrawerNavigator/App.js
+++ 05/DrawerNavigator/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Drawer = createDrawerNavigator();
 
@@ -34,9 +35,31 @@ const App = () => {
         initialRouteName="Home"
         drawerPosition="left"
         backBehavior='history'
+        drawerContent={({navigation}) => (
+          <SafeAreaView>
+            <Text>A Custom Drawer</Text>
+            <Button 
+              onPress={() => navigation.closeDrawer()}
+              title="Drawer 닫기"
+            />
+          </SafeAreaView>
+        )}
+        // screenOptions={{
+        //   drawerActiveBackgroundColor: '#fb8c00', 
+        //   drawerActiveTintColor: 'white', 
+        // }}
       >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Setting" component={SettingScreen} />
+        <Drawer.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{title: '홈'}}
+          // options={{title: '홈', headerLeft: () => <Text>Left</Text>}}
+        />
+        <Drawer.Screen 
+          name="Setting" 
+          component={SettingScreen} 
+          options={{title: '설정'}}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
