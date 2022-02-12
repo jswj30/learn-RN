d8d795192c7d4db2e9e6f01d3ddaf04071b383e4
jswj <jswj30@naver.com>
2022-02-12 15:55:21 +0900

    StatusBar 학습

---
+++ 03/TodoApp_StatusBar/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+
+import DateHead from './components/DateHead';
+
+const App = () => {
+  const today = new Date();
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <DateHead date={today} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default App;
