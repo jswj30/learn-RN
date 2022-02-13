0d797ac472ec66e05561943b8165d04401f5dc52
jswj <jswj30@naver.com>
2022-02-13 14:11:53 +0900

    6.2

--- 06/DayLog/App.js
+++ 06/DayLog/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './screens/RootStack';
-import LogContext from './contexts/LogContext';
+import { LogContextProvider } from './contexts/LogContext';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <LogContext.Provider value="app.js">
-        <RootStack />
-      </LogContext.Provider>
+      <RootStack />
     </NavigationContainer>
   );
 };
--- 06/DayLog/screens/FeedsScreen.js
+++ 06/DayLog/screens/FeedsScreen.js
@@ -1,23 +1,48 @@
-import React, {useCallback, useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import LogContext from '../contexts/LogContext';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
 const FeedsScreen = () => {
-  const value = useContext(LogContext);
   return (
-    <View style={styles.block}>
-      <Text>{value}</Text>
-    </View>
+    <View style={styles.block} />
   );
 };
 
 const styles = StyleSheet.create({
-  box: {
-    borderWidth: 2, 
-    padding: 16, 
-    borderBottomColor: 'black', 
-    marginBottom: 16, 
-  },  
+  block: {
+  }, 
 });
 
 export default FeedsScreen;
+
+// import React, {useCallback, useContext} from 'react';
+// import {StyleSheet, View, Text, TextInput} from 'react-native';
+// import LogContext from '../contexts/LogContext';
+
+// const FeedsScreen = () => {
+//   const {text, setText} = useContext(LogContext);
+//   return (
+//     <View style={styles.block}>
+//       <TextInput 
+//         value={text} 
+//         onChangeText={setText}
+//         placeholder="텍스트를 입력하세요." 
+//         style={styles.input}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   box: {
+//     borderWidth: 2, 
+//     padding: 16, 
+//     borderBottomColor: 'black', 
+//     marginBottom: 16, 
+//   },  
+//   input: {
+//     padding: 16, 
+//     backgroundColor: 'white', 
+//   }, 
+// });
+
+// export default FeedsScreen;
