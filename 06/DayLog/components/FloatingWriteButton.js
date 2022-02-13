265c3f7f566bd401b97ee9aa0313bbed24e5b252
jswj <jswj30@naver.com>
2022-02-13 17:37:13 +0900

    1

---
+++ 06/DayLog/components/FloatingWriteButton.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Platform, Pressable, StyleSheet, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const FloatingWriteButton = () => {
+  return (
+    <View style={styles.wrapper}>
+      <Pressable
+        style={(pressed) => [
+          styles.button, 
+          Platform.OS === 'ios' && {
+            opacity: pressed ? 0.6 : 1, 
+          }, 
+        ]}
+        android_ripple={{color: 'white'}}
+      >
+        <Icon name="add" size={24} style={styles.icon} />
+      </Pressable>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute', 
+    bottom: 16, 
+    right: 16, 
+    width: 56, 
+    height: 56, 
+    borderRadius: 28, 
+    ...Platform.select({
+      ios: {
+        shadowColor: '#4d4d4d', 
+        shadowOffset: {width: 0, height: 4}, 
+        shadowOpacity: 0.3, 
+        shadowRadius: 4, 
+      }, 
+      android: {
+        elevation: 5, 
+      }, 
+    }), 
+    overflow: Platform({
+      android: 'hidden'
+    }), 
+  }, 
+  button: {
+    width: 56, 
+    height: 56, 
+    borderRadius: 28, 
+    backgroundColor: '#009688', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  }, 
+  icon: {
+    color: 'white', 
+  }, 
+});
+
+export default FloatingWriteButton;
