f1eaef106b18cab0680088f5020cf0a084996f52
jswj <jswj30@naver.com>
2022-02-21 10:18:58 +0900

    interpolate

--- 06/DayLog/screens/CalendarScreen.js
+++ 06/DayLog/screens/CalendarScreen.js
@@ -7,7 +7,7 @@ function SlideLeftAndRight() {
 
   useEffect(() => {
     Animated.timing(animation, {
-      toValue: enabled ? 150 : 0, 
+      toValue: enabled ? 1 : 0, 
       useNAtiveDriver: true, 
     }).start();
   }, [enabled, animation])
@@ -18,7 +18,18 @@ function SlideLeftAndRight() {
         style={[
           styles.rectangle, 
           {
-            transform: [{translateX: animation}]
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1], 
+                  outputRange: [0, 150], 
+                }), 
+              }, 
+            ], 
+            opacity: animation.interpolate({
+              inputRange: [0, 1], 
+              outputRange: [1, 0], 
+            }), 
           }, 
         ]}
       />
