947537545b4279ffe721f8070fc14ca25a10471b
jswj <jswj30@naver.com>
2022-02-22 20:56:57 +0900

    06

--- 06/DayLog/components/CalendarView.js
+++ 06/DayLog/components/CalendarView.js
@@ -3,8 +3,29 @@ import {Calendar} from 'react-native-calendars';
 import {StyleSheet} from 'react-native';
 
 const CalendarView = () => {
+  const markedDates = {
+    '2022-02-12': {
+      selected: true, 
+    }, 
+    '2022-02-18': {
+      marked: true, 
+    }, 
+    '2022-02-19': {
+      marked: true, 
+    }, 
+  };
+
   return (
-    <Calendar style={styles.calendar} />
+    <Calendar 
+      style={styles.calendar} 
+      markedDates={markedDates}
+      theme={{
+        selectedDayBackgroundColor: '#009688', 
+        arrowColor: '#009688', 
+        dotColor: '#009688', 
+        todayTextColor: '#009688', 
+      }}  
+    />
   );
 };
 
