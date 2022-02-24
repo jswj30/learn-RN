59418bc9338dc8f2af37b4bf779d801bfdf8eeff
jswj <jswj30@naver.com>
2022-02-24 09:19:07 +0900

    7

--- 06/DayLog/contexts/LogContext.js
+++ 06/DayLog/contexts/LogContext.js
@@ -1,19 +1,12 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect, useRef} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/logsStorage';
 
 const LogContext = createContext();
 
 export function LogContextProvider({children}) {
-  const [logs, setLogs] = useState(
-    Array.from({length: 10})
-    .map((_, index) => ({
-      id: uuidv4(), 
-      title: `Log ${index}`, 
-      body: `log ${index}`, 
-      date: new Date().toISOString(), 
-    }))
-    .reverse()
-  );
+  const initialLogsRef = useRef(null);
+  const [logs, setLogs] = useState([]);
 
   const onCreate = ({title, body, date}) => {
     const log = {
@@ -36,6 +29,23 @@ export function LogContextProvider({children}) {
     const nextLogs = logs.filter((log) => log.id !== id);
     setLogs(nextLogs);
   };
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
 
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
