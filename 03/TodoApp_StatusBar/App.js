f42de4477b711daea49a54c732fed799576c1cf0
jswj <jswj30@naver.com>
2022-03-01 18:52:36 +0900

    1

--- 03/TodoApp_StatusBar/App.js
+++ 03/TodoApp_StatusBar/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 const App = () => {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true}, 
+    {id: 2, text: '리액트 네이티브  기초 공부', done: false}, 
+    {id: 3, text: '투두리스트 만들어보기', done: false}, 
+  ]);
+
+  const onInsert = (text) => {
+    const nextId = todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId, 
+      text, 
+      done: false, 
+    };
+    setTodos(todos.concat(todo));
+  };
 
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
         <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          behavior={Platform.select({ios: 'padding'})}
           style={styles.avoid}
         >
           <DateHead date={today} />
-          <Empty />
-          <AddTodo />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
