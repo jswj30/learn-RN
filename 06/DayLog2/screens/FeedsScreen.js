ce30c9475be890e67f67d19a6a8306c2a7bb4d78
jswj <jswj30@naver.com>
2022-04-04 20:39:46 +0900

    6.2.1

--- 06/DayLog2/screens/FeedsScreen.js
+++ 06/DayLog2/screens/FeedsScreen.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import LogContext from '../contexts/LogContext';
 
 const FeedsScreen = () => {
   return (
-    <View style={styles.block} />
+    <View style={styles.block}>
+      <Box>
+        {(value) => <Text>{value}</Text>}
+      </Box>
+    </View>
   );
-};
+}
+
+function Box({children}) {
+  return <View style={styles.box}>{children('Hello World')}</View>;
+}
 
 const styles = StyleSheet.create({
   block: {
 
   }, 
+  box: {
+    borderWidth: 2, 
+    padding: 16, 
+    borderBottomColor: 'black', 
+    marginBottom: 16, 
+  }, 
 })
 export default FeedsScreen;
+
+
+// const FeedsScreen = () => {
+//   return (
+//     <View style={styles.block}>
+//       <LogContext.Consumer>
+//         {(value) => <Text>{value}</Text>}
+//       </LogContext.Consumer>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   block: {
+
+//   }, 
+// })
+// export default FeedsScreen;
