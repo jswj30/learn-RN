74fd70280d8529f3565a7b2e1297757464c28996
jswj <jswj30@naver.com>
2022-04-04 20:00:35 +0900

    6.1

--- 06/DayLog2/App.js
+++ 06/DayLog2/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import RootStack from './screens/RootStack';
 
 const App = () => {
   return (
-    <NavigationContainer></NavigationContainer>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 };
 
---
+++ 06/DayLog2/screens/FeedsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const FeedsScreen = () => {
+  return (
+    <View style={styles.block} />
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+
+  }, 
+})
+export default FeedsScreen;
