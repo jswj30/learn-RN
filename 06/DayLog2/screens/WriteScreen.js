ef1abb5112725f7c601037aa6955646efbe253c0
jswj <jswj30@naver.com>
2022-04-08 07:35:09 +0900

    6.3.5

--- 06/DayLog2/screens/WriteScreen.js
+++ 06/DayLog2/screens/WriteScreen.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import WriteHeader from '../components/WriteHeader';
 import WriteEditor from '../components/WriteEditor';
 
 const WriteScreen = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <SafeAreaView style={styles.block}>
-      <WriteHeader />
-      <WriteEditor />
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <WriteHeader />
+        <WriteEditor 
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -17,6 +30,9 @@ const styles = StyleSheet.create({
   block: {
     flex: 1, 
     backgroundColor: 'white', 
+  }, 
+  avoidingView: {
+    flex: 1, 
   }, 
 })
 export default WriteScreen;
