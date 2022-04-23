db6b508ef8ad70048637ae9a35f575404a9d4dd4
jswj <jswj30@naver.com>
2022-04-23 16:48:57 +0900

    7.6

--- 06/DayLog2/components/WriteHeader.js
+++ 06/DayLog2/components/WriteHeader.js
@@ -1,14 +1,37 @@
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
-import React from 'react';
 import { Pressable, StyleSheet, View, Text } from 'react-native';
 import TransparentCircleButton from './TransparentCircleButton';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const WriteHeader = ({onSave, onAskRemove, isEditing, date, onChangeDate}) => {
   const navigation = useNavigation();
   const onGoBack = () => {
     navigation.pop();
+  }
+
+  const [mode, setMode] = useState('date');
+  const [visible, setVisible] = useState(false);
+
+  const onPressDate = () => {
+    setMode('date');
+    setVisible(true);
+  };
+
+  const onPressTime = () => {
+    setMode('time');
+    setVisible(true);
+  }
+
+  const onConfirm = (selectedDate) => {
+    setVisible(false);
+    onChangeDate(selectedDate);
+  }
+
+  const onCancel = () => {
+    setVisible(false);
   }
 
   return (
@@ -36,7 +59,7 @@ const WriteHeader = ({onSave, onAskRemove, isEditing, date, onChangeDate}) => {
         />
       </View>
       <View style={styles.center}>
-        <Pressable>
+        <Pressable onPress={onPressDate}>
           <Text>
             {format(new Date(date), 'PPP', {
               locale: ko, 
@@ -44,10 +67,17 @@ const WriteHeader = ({onSave, onAskRemove, isEditing, date, onChangeDate}) => {
           </Text>
         </Pressable>
         <View style={styles.separator} />
-        <Pressable>
+        <Pressable onPress={onPressTime}>
           <Text>{format(new Date(date), 'p', {locale: ko})}</Text>
         </Pressable>
       </View>
+      <DateTimePickerModal 
+        isVisible={visible}
+        mode={mode}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+        date={date}
+      />
     </View>
   );
 };
