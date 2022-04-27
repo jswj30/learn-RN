0b12c4f14751c37edff5a7ef574bdb533f29e640
jswj <jswj30@naver.com>
2022-04-27 21:52:22 +0900

    8.2.6

--- 08/PublicGalleryJswj/screens/SignInScreen.js
+++ 08/PublicGalleryJswj/screens/SignInScreen.js
@@ -29,15 +29,28 @@ const SignInScreen = ({navigation, route}) => {
 
   const onSubmit = async () => {
     Keyboard.dismiss();
-    const {email, password} = form;
-    const info = {email, password};
+    const {email, password, confirmPassword} = form;
+
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
     setLoading(true);
+    const info = {email, password};
+
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
       console.log(user);
     } catch (e) {
-      Alert.alert('실패');
-      console.log(e);
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.', 
+        'auth/wrong-password': '잘못된 비밀번호입니다.', 
+        'auth/user-not-found': '존재하지 않는 계정입니다.', 
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.', 
+      };
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
     } finally {
       setLoading(false);
     }
