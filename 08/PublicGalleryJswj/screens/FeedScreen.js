8566745fdc494c4c20a6c16f271b96f845fd6fdc
jswj <jswj30@naver.com>
2022-05-14 17:06:19 +0900

    1

--- 08/PublicGalleryJswj/screens/FeedScreen.js
+++ 08/PublicGalleryJswj/screens/FeedScreen.js
@@ -1,21 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, ActivityIndicator, StyleSheet } from 'react-native';
-import { getPosts } from '../lib/posts';
+import { getPosts, getOlderPosts, PAGE_SIZE } from '../lib/posts';
 
 import PostCard from '../components/PostCard';
 
 const FeedScreen = () => {
   const [posts, setPosts] = useState(null);
+  const [noMorePost, setNoMorePost] = useState(false);
 
   useEffect(() => {
     getPosts().then(setPosts);
   }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
 
   return (
     <FlatList 
       data={posts}
       renderItem={renderItem}
       keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      } 
     />
   );
 };
