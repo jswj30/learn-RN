15c8a21fd415beb0a62294446c5545e38c28625c
jswj <jswj30@naver.com>
2022-05-17 07:48:17 +0900

    1

--- 08/PublicGalleryJswj/screens/FeedScreen.js
+++ 08/PublicGalleryJswj/screens/FeedScreen.js
@@ -1,44 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, ActivityIndicator, StyleSheet, RefreshControl } from 'react-native';
-import { getPosts, getOlderPosts, PAGE_SIZE, getNewerPosts } from '../lib/posts';
 
 import PostCard from '../components/PostCard';
+import usePosts from '../hooks/usePosts';
 
 const FeedScreen = () => {
-  const [posts, setPosts] = useState(null);
-  const [noMorePost, setNoMorePost] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-
-  useEffect(() => {
-    getPosts().then(setPosts);
-  }, []);
-
-  const onLoadMore = async () => {
-    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
-      return;
-    }
-    const lastPost = posts[posts.length - 1];
-    const olderPosts = await getOlderPosts(lastPost.id);
-    if (olderPosts.length < PAGE_SIZE) {
-      setNoMorePost(true);
-    }
-    setPosts(posts.concat(olderPosts));
-  };
-
-  const onRefresh = async () => {
-    if (!posts || posts.length === 0 || refreshing) {
-      return;
-    }
-    const firstPost = posts[0];
-    setRefreshing(true);
-    const newerPosts = await getNewerPosts(firstPost.id);
-    setRefreshing(false);
-    if (newerPosts.length === 0) {
-      return;
-    }
-    setPosts(newerPosts.concat(posts));
-  }
-
+  const {posts, noMorePost, refreshing, onLoadMore, onRefresh} = usePosts();
   return (
     <FlatList 
       data={posts}
