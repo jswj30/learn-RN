4396872d138ae2ea85948be8d36e2af353a22f49
jswj <jswj30@naver.com>
2022-05-17 21:18:39 +0900

    9.5.7

--- 08/PublicGalleryJswj/components/Profile.js
+++ 08/PublicGalleryJswj/components/Profile.js
@@ -2,54 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { 
   ActivityIndicator, 
   FlatList, 
-  Image, 
   StyleSheet, 
   Text, 
   View, 
   RefreshControl, 
 } from 'react-native';
-import { getPosts, getNewerPosts, getOlderPosts, PAGE_SIZE } from '../lib/posts';
 import { getUser } from '../lib/user';
 
 import Avatar from './Avatar';
 import PostGridItem from './PostGridItem';
+import usePosts from '../hooks/usePosts';
 
 const Profile = ({userId}) => {
+  const {posts, noMorePost, refreshing, onLoadMore, onRefresh} = usePosts(userId);
+
   const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState(null);
-
-  const [noMorePost, setNoMorePost] = useState(false);
-  const [refreshing, setRefreshing] = useState(false);
-  
-  const onLoadMore = async () => {
-    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
-      return;
-    }
-    const lastPost = posts[posts.length - 1];
-    const olderPosts = await getOlderPosts(lastPost.id, userId);
-    if (olderPosts.length < PAGE_SIZE) {
-      setNoMorePost(true);
-    }
-    setPosts(posts.concat(olderPosts));
-  }
-
-  const onRefresh = async () => {
-    if (!posts || posts.length === 0 || refreshing) {
-      return;
-    }
-    const firstPost = posts[0];
-    setRefreshing(true);
-    const newerPosts = await getNewerPosts(firstPost.id, userId);
-    setRefreshing(false);
-    if (newerPosts.length === 0) {
-      return;
-    }
-    setPosts(newerPosts.concat(posts));
-  }
 
   useEffect(() => {
     getUser(userId).then(setUser);
-    getPosts({userId}).then(setPosts);
   }, [userId]);
 
   if (!user || !posts) {
--- 08/PublicGalleryJswj/screens/FeedScreen.js
+++ 08/PublicGalleryJswj/screens/FeedScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FlatList, ActivityIndicator, StyleSheet, RefreshControl } from 'react-native';
 
 import PostCard from '../components/PostCard';
