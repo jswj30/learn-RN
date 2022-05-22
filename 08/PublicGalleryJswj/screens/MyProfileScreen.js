a48895177877a4ca7db5f00023842e1d47b0b6a9
jswj <jswj30@naver.com>
2022-05-22 18:57:58 +0900

    9.9

--- 08/PublicGalleryJswj/screens/MyProfileScreen.js
+++ 08/PublicGalleryJswj/screens/MyProfileScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useUserContext } from '../contexts/UserContext';
 
 import Profile from '../components/Profile';
-import { useUserContext } from '../contexts/UserContext';
+import IconRightButton from '../components/IconRightButton';
 
 const MyProfileScreen = () => {
   const {user} = useUserContext();
@@ -11,6 +12,12 @@ const MyProfileScreen = () => {
   useEffect(() => {
     navigation.setOptions({
       title: user.displayName, 
+      headerRight: () => (
+        <IconRightButton 
+          name="settings"
+          onPress={() => navigation.push('Setting')}
+        />
+      )
     });
   }, [navigation, user]);
 
--- 08/PublicGalleryJswj/screens/RootStack.js
+++ 08/PublicGalleryJswj/screens/RootStack.js
@@ -9,6 +9,7 @@ import WelcomeScreen from './WelcomeScreen';
 import MainTab from './MainTab';
 import UploadScreen from './UploadScreen';
 import ModifyScreen from './ModifyScreen';
+import SettingScreen from './SettingScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -47,6 +48,11 @@ const RootStack = () => {
             name="Modify"
             component={ModifyScreen}
             options={{title: '설명 수정', headerBackTitle: '뒤로가기'}}
+          />
+          <Stack.Screen 
+            name="Setting"
+            component={SettingScreen}
+            options={{title: '설정', headerBackTitle: '뒤로가기'}}
           />
         </>
       ) : (
