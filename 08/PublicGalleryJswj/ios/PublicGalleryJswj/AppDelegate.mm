3ea1e729aaa79ab83bad9ef6829b9179139de08f
jswj <jswj30@naver.com>
2022-05-22 20:10:36 +0900

    9.10.2

--- 08/PublicGalleryJswj/ios/PublicGalleryJswj/AppDelegate.mm
+++ 08/PublicGalleryJswj/ios/PublicGalleryJswj/AppDelegate.mm
@@ -4,6 +4,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNSplashScreen.h>
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -63,6 +64,9 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  [RNSplashScreen show];
+  
   return YES;
 }
 
